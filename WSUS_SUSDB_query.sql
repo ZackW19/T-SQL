2cee692fb85c73d6ec9ab9f90377e34bd199853c
ZackW19 <30461415+ZackW19@users.noreply.github.com>
2017-07-26 10:16:57 +0200

    Update WSUS_SUSDB_query.sql

--- WSUS_SUSDB_query.sql
+++ WSUS_SUSDB_query.sql
@@ -1,13 +1,13 @@
 USE SUSDB
 SELECT C.FullDomainName AS Fqdn,C.LastSyncTime, C.LastSyncResult, C.LastReportedStatusTime ,TG.Name AS [GroupName], VU.KnowledgebaseArticle AS KB, VU.SecurityBulletin, VU.DefaultTitle, VU.InstallationImpact, VU.PublicationState, VU.MsrcSeverity, VU.ArrivalDate AS [Imported_Arrival_Date], VU.CreationDate, VU.IsDeclined, VU.IsWsusInfrastructureUpdate, U.IsPublic, VUA.Action, VUA.AdministratorName, VUA.CreationDate AS [DeploymentTime],
        'State' = CASE 
-	   WHEN UP.SummarizationState = 0 THEN 'Unknown' 
+       WHEN UP.SummarizationState = 0 THEN 'Unknown' 
        WHEN UP.SummarizationState = 1 THEN 'Not Applicable' 
        WHEN UP.SummarizationState = 2 THEN 'Not Installed'  
        WHEN UP.SummarizationState = 3 THEN 'Downloaded'
        WHEN UP.SummarizationState = 4 THEN 'Installed'
        WHEN UP.SummarizationState = 5 THEN 'Failed'
-	   WHEN UP.SummarizationState = 6 THEN 'Installed Pending Reboot'
+       WHEN UP.SummarizationState = 6 THEN 'Installed Pending Reboot'
        END
 
 FROM tbComputerTarget C
